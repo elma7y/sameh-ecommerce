b629b1a9c463400329f2ca0ae31dfe0c74be0df0
sameh <elma7y1996@gmail.com>
2023-06-22 23:38:10 +0300

    update-email

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,6 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
